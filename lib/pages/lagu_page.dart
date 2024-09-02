d52ec8f082192dbf8856593035f5e84fc7d7ff20
dauzkepow <dauzcool20@gmail.com>
2024-09-02 15:59:21 +0700

    1:10:03

---
+++ lib/pages/lagu_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lagu_daerah_app/data/datasources/lagu_remote_datasource.dart';
+
+import '../data/models/lagu_response_model.dart';
+
+class LaguPage extends StatefulWidget {
+  const LaguPage({super.key});
+
+  @override
+  State<LaguPage> createState() => _LaguPageState();
+}
+
+class _LaguPageState extends State<LaguPage> {
+  //ambil data dari lagu_response_model.dart
+  late Future<LaguResponseModel> laguDaerahList;
+
+  //initstate
+  @override
+  void initState() {
+    laguDaerahList = LaguRemoteDatasource().getLaguDaerah();
+    super.initState();
+  }
+
+  //yang disarankan flutter pakai Future
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Lagu Daerah',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        elevation: 2,
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: FutureBuilder<LaguResponseModel>(
+        future: laguDaerahList,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(snapshot.data!.data.data[index].judul),
+                    subtitle: Text(snapshot.data!.data.data[index].daerah),
+                    leading: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                  ),
+                );
+              },
+              itemCount: snapshot.data!.data.data.length,
+            );
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          return Center(
+            child: const CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
